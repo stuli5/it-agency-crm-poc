6f396ac1a777cf2e5bef535c5c4a2f689add6a20
jaro <jaro@jaro--MacBook-Air.local>
2025-09-25 11:19:24 +0200

    oprava index

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
